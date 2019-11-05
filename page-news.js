d1d14defb24beb03ddee4147a9d81e2e531efeb6
Alexander Schmidt <alexander116@gmx.de>
2019-11-05 16:45:22 +0100

    Änderung

---
+++ page-news.js
@@ -0,0 +1,15 @@
+class PageNews {
+    constructor(app) {
+        this._app = app;
+    }
+    async show() {
+        console.log("Newsseite");
+        let html = await fetch("page-news.html");
+
+        let htmlContent = "";
+        if (html.ok) {
+            htmlContent = await html.text();
+        }
+        this._app.setPageContent(htmlContent);
+    }
+}
